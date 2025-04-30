ce782f4bbcaaede65b8eb98e8b3075bb786c8fb3
DaffaJatmiko <daffamiko123@gmail.com>
2025-04-30 10:27:53 +0700

    Add Praktikum 5 Page

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,19 @@ import LandingPage from './pages/LandingPage';
 // import JSXPresentation from "./pages/p7/test2";
 // import ComponentsPropsStatePresentation from "./pages/p7/test3";
 // import ReactLifecyclePresentation from "./pages/p7/test4";
-import ReactRenderingPresentation from "./pages/p7/ReactRenderingPresentation";
-import UTSReact from "./pages/UTS";
-import JavaScriptNextGen from "./pages/NextGenJavascript";
-import ReactDasar from "./pages/ReactDasar";
+import ReactRenderingPresentation from './pages/p7/ReactRenderingPresentation';
+import UTSReact from './pages/UTS';
+import JavaScriptNextGen from './pages/NextGenJavascript';
+import ReactDasar from './pages/ReactDasar';
 // import PengantarPythonPresentation from "./pages/p9/PengantarPythonPresentation";
 // import PythonOperatorPresentation from "./pages/p9/PythonOperatorPresentation";
 // import TipeDataPythonPresentation from "./pages/p9/TipeDataPythonPresentation";
-import PythonControlFlowPresentation from "./pages/p9/PythonControlFlowPresentation";
-import PythonOOPPresentation from "./pages/p10/PythonOOPPresentation";
-import { PythonClassObjectPresentation } from "./pages/p10/test";
-import PythonAttributePresentation from "./pages/p10/test2";
+import PythonControlFlowPresentation from './pages/p9/PythonControlFlowPresentation';
+import PythonOOPPresentation from './pages/p10/PythonOOPPresentation';
+import { PythonClassObjectPresentation } from './pages/p10/test';
+import PythonAttributePresentation from './pages/p10/test2';
+import PythonDasar from './pages/PythonDasar';
+import PythonOOPPraktikum from './pages/PythonOOPPraktikum';
 
 function App() {
   return (
@@ -48,6 +50,10 @@ function App() {
           <Route
             path="pemrograman-web/pemrograman-python"
             element={<PythonDasar />}
+          />
+          <Route
+            path="/pemrograman-web/OOPPython"
+            element={<PythonOOPPraktikum />}
           />
         </Routes>
       </BrowserRouter>
